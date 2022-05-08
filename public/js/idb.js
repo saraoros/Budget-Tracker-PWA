0487513dc1e3d256165d2bfdd3693fed402b2478
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-05-08 15:19:00 -0700

    idb file completed, errors fixed

---
+++ public/js/idb.js
@@ -0,0 +1,64 @@
+let db;
+
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  db.createObjectStore("new_budget", { autoIncrement: true });
+};
+request.onsuccess = function (e) {
+  db = e.target.result;
+
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_budget"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_budget");
+
+  budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+  const transaction = db.transaction(["new_budget"], "readwrite");
+  const budgetObjectStore = transaction.objectStore("new_budget");
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["new_budget"], "readwrite");
+
+          const budgetObjectStore = transaction.objectStore("new_budget");
+
+          budgetObjectStore.clear();
+
+          alert("All budget information has been updated!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadBudget);
