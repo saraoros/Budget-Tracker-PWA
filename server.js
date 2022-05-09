ef340ba553ad2c78a99a93debe2f1ba365006bc0
Sara Oros Gochez <saraorosgochez@Saras-MacBook-Air.local>
2022-05-08 20:31:10 -0700

    deploying

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget-tracker";
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
@@ -16,10 +24,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
